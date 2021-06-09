d820272bd567f73336591e5ce3b58efa99005a81
David Rafe <david.rafe@gmail.com>
2021-06-09 15:38:19 -0400

    commit tree a649814fd72e3ec7db3dff54eba5d4dbd7fdb817

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions here
+function map(array, func) {
+    let arr = []
+    for(let i = 0; i < array.length; i++){
+       arr.push(func(array[i]))
+    }
+    return arr  
+}
+
+
+// function reduce(array, func, value){
+//     let accum = value 
+//     for(let i = 0; i < array.length; i++){
+//       accum = func(array[i], accum) 
+//     }
+//     return accum 
+// } 
+
+function reduce(src, callback, startingValue) {
+    let total;
+    if (startingValue) {
+      total = startingValue;
+      for (let i = 0; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      return total;
+    } else {
+      total = src[0];
+      for (let i = 1; i < src.length; i++) {
+        total = callback(src[i], total);
+      }
+      return total;
+    }
+  }
